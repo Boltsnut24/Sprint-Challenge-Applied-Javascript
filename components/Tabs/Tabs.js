d921f3e4820d0cc06cdce5875be3fea244bb50a7
Brad <Brad@BRADSPC>
2019-06-21 10:32:39 -0600

    Added top secret tab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,28 +6,28 @@ class TabLink {
     // Get the `data-tab` value from this.tabElement and store it here
      this.tabData = this.tabElement.dataset.tab;
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
-    console.log(this.tabData);
 
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
       this.cards = document.querySelectorAll('.card');
-      console.log(this.cards);
+    }
+    else if (this.tabData === 'topSecret'){
+      this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
       this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);       
     }
-   console.log(this.cards);
+
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     this.cards = Array.from(this.cards).map( card => new TabCard(card));
     
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', () => this.selectTab());
-    console.log('-------');
   }
 
   selectTab(){ 
-    console.log('click');
+
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll('.tab');
     
@@ -51,9 +51,7 @@ class TabLink {
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    this.cardElement = cardElement;
-    console.log(this.cardElement);
-    
+    this.cardElement = cardElement;   
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
