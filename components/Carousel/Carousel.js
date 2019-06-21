5a71a79aea5fd4c86c2c734689893af43ff73880
Brad <Brad@BRADSPC>
2019-06-21 11:15:31 -0600

    Finished slideshow version of carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,54 @@
 class Carousel {
+    constructor(carousel){
+        this.carousel = carousel;
+        this.leftBtn = document.querySelector('.left-button');
+        this.rightBtn = document.querySelector('.right-button');
+
+        this.images = this.carousel.querySelectorAll('img');
+
+        this.leftBtn.addEventListener('click', () => this.scrollLeft());
+        this.rightBtn.addEventListener('click', () => this.scrollRight());
+
+        this.index = 0;
+    }
+
+    scrollLeft(){
+        let imgArr = Array.from(this.images);
+        console.log(imgArr);
+        document.slide.src = imgArr[0];
+
+        if( this.index > 0 ){
+            console.log(imgArr[this.index]);
+            this.index--;
+        } else {
+            this.index = imgArr.length - 1;
+        }
+        
+        imgArr.forEach(photo => photo.style.display = 'none');
+
+        imgArr[this.index].style.display = 'block';
+    }
+
+    scrollRight(){
+        let imgArr = Array.from(this.images);
+        console.log(imgArr);
+        document.slide.src = imgArr[0];
+
+        if( this.index < imgArr.length - 1){
+            console.log(imgArr[this.index]);
+            this.index++;
+        } else {
+            this.index = 0;
+        }
+        imgArr.forEach(photo => photo.style.display = 'none');
+
+        imgArr[this.index].style.display = 'block';
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
